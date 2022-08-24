55721bd1a474c6c2ff208522cbd665be595ce224
Andrej Simeunovic <andrej.simeunovic98@gmail.com>
2022-08-24 15:51:07 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ function Navbar() {
 function Footer() {
   return (
     <footer className="pt-3 mt-4 text-muted border-top">
-      EDAF90 - webprogrammering
+      Andrej Simeunovic
     </footer>
   );
 }
